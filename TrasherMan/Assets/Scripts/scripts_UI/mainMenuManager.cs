d22b8be0b28a67f7419d9f46140d4b5974b0ae76
Abraar Sadek <abraarsadek@gmail.com>
2026-04-08 17:10:57 -0400

    Main Menu Manager Script

---
+++ TrasherMan/Assets/Scripts/scripts_UI/mainMenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//mainMenuManager Class - Manages the main menu UI interactions
+public class mainMenuManager : MonoBehaviour {
+
+    public void OnPlayButtonPress() {
+
+        Debug.Log("Play Button Pressed"); //Logs to the console that the play button was pressed
+    
+    } //End of OnPlayButtonPress Method
+
+    //OnQuitButtonPress Method - Called when the quit button is pressed
+    public void OnQuitButtonPress() {
+
+        Debug.Log("Quit Button Pressed"); //Logs to the console that the quit button was pressed
+        Application.Quit(); //Quits the application when the quit button is pressed
+
+        //Exit Play Mode iftesting in the Unity Editor
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+
+    } //End of OnQuitButtonPress Method
+
+} //End of mainMenuManager Class
